7a6686c90feeef25f7cc915a880c13bd84f9acac
Zach M <wackizacki@gmail.com>
2020-04-06 03:18:30 -0400

    updated

--- snake.lua
+++ snake.lua
@@ -1,1 +1,45 @@
-print("dab")
+local component = require("component")
+local gpu = component.gpu
+local keyboard = require("keyboard")
+
+local UP = 0
+local DOWN = 1
+local LEFT = 2
+local RIGHT = 3
+
+local x = 10
+local y = 10
+local dir = RIGHT
+
+gpu.setForeground(0x000000)
+gpu.setBackground(0xFFFFFF)
+
+while true do
+  os.sleep(0.1)
+  if keyboard.isControlDown() and keyboard.isKeyDown(keyboard.keys.c) then
+    break
+  end
+
+  gpu.fill(x,y," ")
+  if keyboard.isKeyDown(keyboard.keys.w) then
+    dir = UP
+  elseif keyboard.isKeyDown(keyboard.keys.s) then
+    dir = DOWN
+  elseif keyboard.isKeyDown(keyboard.keys.a) then
+    dir = LEFT
+  elseif keyboard.isKeyDown(keyboard.keys.d) then
+    dir = RIGHT
+  end
+
+  if dir == UP then
+    y = y-1
+  elseif dir == DOWN then
+    y = y+1
+  elseif dir == LEFT then
+    x = x-1
+  elseif dir == RIGHT then
+    x = x+1
+  end
+
+  gpu.fill(x,y,"X")
+end
